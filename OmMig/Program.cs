c9ffe05c4c79e70d6427530830adeb466b1ecdae
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-01-13 09:40:25 +0100

    Add project files.

---
+++ OmMig/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OmMig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // "\n" laver en ny linje
+            Console.WriteLine("Navn: Jens Zakarias Pedersen \nAlder: 19 år \nYnglingsspil: Intet bestemt \nJeg er elev på AspIT");
+            Console.ReadLine();
+        }
+    }
+}
